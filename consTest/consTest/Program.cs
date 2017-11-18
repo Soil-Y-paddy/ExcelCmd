a1aa165ca185daa42241c305c74ba4350cde44aa
Soil.Y Paddy field <soil_y@hotmail.co.jp>
2017-11-18 17:47:15 +0900

    コメントを追加

--- consTest/consTest/Program.cs
+++ consTest/consTest/Program.cs
@@ -6,11 +6,11 @@ namespace consTest
 	{
 		static void Main(string[] args)
 		{
-			string strLine = Console.ReadLine();
-			char[] aryChar = strLine.ToCharArray();
-			for(int nNum = aryChar.Length - 1; nNum > -1; nNum--)
+			string strLine = Console.ReadLine(); // 標準入力から1行取得
+			char[] aryChar = strLine.ToCharArray(); // 文字配列に変換
+			for(int nNum = aryChar.Length - 1; nNum > -1; nNum--) // 逆順ループ
 			{
-				Console.Write(aryChar[nNum]);
+				Console.Write(aryChar[nNum]);//一文字づつ書き出す
 			}
 		}
 	}
