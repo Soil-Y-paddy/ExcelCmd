66b2e40b88dcb02274939c25f14a66ec50f278fd
Soil.Y Paddy field <soil_y@hotmail.co.jp>
2017-11-18 16:41:27 +0900

    テスト用コンソールアプリ
    言語：C#
    内容：標準入力された文字列を、逆順にして標準出力に返す
    　　　※入力文字列は、半角英数のみ対応

---
+++ consTest/consTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string strLine = Console.ReadLine();
+			char[] aryChar = strLine.ToArray();
+			for(int nNum = aryChar.Length - 1; nNum > -1; nNum--)
+			{
+				Console.Write(aryChar[nNum]);
+			}
+			Console.WriteLine();
+		}
+	}
+}
