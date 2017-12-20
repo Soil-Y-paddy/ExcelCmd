2aaee5c7f8e64786c1570eadea1ebf44fd9eed4e
y-tsuchida <soil_y@hotmail.co.jp>
2017-12-20 20:52:25 +0900

    komit

--- consTest/consTest/Program.cs
+++ consTest/consTest/Program.cs
@@ -6,6 +6,7 @@ namespace consTest
 	{
 		static void Main(string[] args)
 		{
+            // Visual Stdio から編集
 			string strLine = Console.ReadLine(); // 標準入力から1行取得
 			char[] aryChar = strLine.ToCharArray(); // 文字配列に変換
 			for(int nNum = aryChar.Length - 1; nNum > -1; nNum--) // 逆順ループ
